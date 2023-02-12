70e76555d68c6c74b416b7b610d327809ffafbef
James Roberts <JRoberts86@gmail.com>
2023-02-12 01:40:57 +0000

    Update files

--- hopfield.py
+++ hopfield.py
@@ -12,7 +12,7 @@ class hopfield():
             memory = np.array([np.where(sample > threshold, 1, -1)]) # Binary dipole
             delta_weights = memory.T * memory
             self.weights += delta_weights
-            np.fill_diagonal(self.weights, 0) # Symmetrical weights
+            # np.fill_diagonal(self.weights, 0) # Symmetrical weights
         self.weights /= data.shape[0] # Normalise against dataset
 
     def infer(self, state, units):
@@ -20,15 +20,18 @@ class hopfield():
             rand_idx = np.random.randint(1, self.ndim)
             spin = np.dot(self.weights[rand_idx,:], state) # Activation function
 
-            state[rand_idx] = 1 if spin > 0 else -1 # Output function
+            # state[rand_idx] = self.step(spin) # Output function
 
-            # prob = self.sigmoid(spin)
-            # state[rand_idx] = 1 if np.random.uniform(0, 1) < prob else -1 # Bernoulli sampling
+            prob = self.sigmoid(spin)
+            state[rand_idx] = 1 if np.random.uniform(0, 1) < prob else -1 # Bernoulli sampling
 
         return state
 
-    def sigmoid(self, x):
+    def step(self, y):
+        return 1 if y > 0 else -1
+
+    def sigmoid(self, x): # Contiuous activation
         return 1.0 / (1.0 + np.exp(-x))
 
-    def compute_energy(self, state): # Original Paper
+    def compute_energy(self, state): # As per original paper
         return -0.5 * np.dot(np.dot(self.weights, state), state.T)
