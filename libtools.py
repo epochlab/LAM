c7d5cda8e0d625bf3d1239407fb5efb88293c8b1
James Roberts <JRoberts86@gmail.com>
2023-06-24 21:58:16 +0100

    Remove plot gradient

--- libtools.py
+++ libtools.py
@@ -161,11 +161,3 @@ def render(x, step, out, col='magma'):
             seq.append(plt.colormaps[col](f) * 255.0)
     imageio.mimsave(out, np.uint8(seq))
     return print("RenderIO Complete")
-
-def plot_gradient(y, colormap, sz=10, edge_width=0.5):
-    x = np.arange(len(y))
-    y_norm = y + abs(y.min())
-    y_norm *= 1/y_norm.max()
-    colors = plt.colormaps[colormap](y_norm)
-    for i in range(len(x) - 1):
-        plt.plot(x[i], y[i], '.', color=colors[i], mec='k', ms=sz, mew=edge_width)
