9b1abb158328b87b52fd541e85edf0fc5284cfae
James Roberts <JRoberts86@gmail.com>
2023-07-24 00:30:18 +0100

    Change gabor sigmaY value =10, add requirements.txt, removed saved matrices

--- libtools.py
+++ libtools.py
@@ -132,7 +132,7 @@ def gabor_conv(img, src, step=20, k_size=5):
         for j in range(src.shape[1]):
             rad = src[i][j] - np.pi # -π and π
             deg = np.rad2deg(rad)
-            kernel = gabor_filter(1, 2, deg, samples=step, min=-k_size, max=k_size) # Orientation
+            kernel = gabor_filter(1, 10, deg, samples=step, min=-k_size, max=k_size) # Orientation
             patch = pad_im[i:i+step, j:j+step]
             features[int(i+pad),int(j+pad)] = np.sum(patch * kernel) # Firing Rate / Response
 
